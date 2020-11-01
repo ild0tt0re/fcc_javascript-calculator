875a922e930c9da77395542fa9e83149573fa1e4
ild0tt0re <ing.annunziata@gmail.com>
2020-11-01 19:25:04 +0100

    draw calculator markup and style

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,37 @@
 import './styles/App.scss';
 
 function App() {
-
-
   return (
     <div className="App">
-      
+      <div id="calculator">
+        <div id="display">
+          <div className="input">
+            0
+          </div>
+          <div className="expression">
+            5+4=9
+          </div>
+        </div>
+        <div className="keyboard">
+          <button id="clear">AC</button>
+          <button id="divide">/</button>
+          <button id="multiply">x</button>
+          <button id="seven">7</button>
+          <button id="eight">8</button>
+          <button id="nine">9</button>
+          <button id="subtract">-</button>
+          <button id="four">4</button>
+          <button id="five">5</button>
+          <button id="six">6</button>
+          <button id="add">+</button>
+          <button id="one">1</button>
+          <button id="two">2</button>
+          <button id="three">3</button>
+          <button id="equals">=</button>
+          <button id="zero">0</button>
+          <button id="decimal">,</button>
+        </div>
+      </div>
     </div>
   );
 }
